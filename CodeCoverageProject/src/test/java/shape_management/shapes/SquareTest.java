a736f8ce49b57cca0bf902c73ceb4181755e89e1
amirmosio <amir.mn.mosio@gmail.com>
2022-05-27 21:35:36 +0430

    Define SquareTest class

--- CodeCoverageProject/src/test/java/shape_management/shapes/SquareTest.java
+++ CodeCoverageProject/src/test/java/shape_management/shapes/SquareTest.java
@@ -1,11 +1,38 @@
 package shape_management.shapes;
 
 import org.junit.Test;
+import shape_management.exceptions.InvalidSizeForCalculateAreaException;
+
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertTrue;
 
 
 public class SquareTest {
     @Test
-    public void testCalculateArea() {
+    public void testCalculateAreaWithValidSize() {
+        Square square = new Square(5);
+        double area = square.calculateArea();
+        assertEquals(area, 5 * 5, 0.00001);
+    }
 
+
+    @Test
+    public void testCalculateAreaWithInvalidSize() {
+        Square square = new Square(-5);
+        boolean exception = false;
+        try {
+            double area = square.calculateArea();
+        } catch (InvalidSizeForCalculateAreaException e) {
+            exception = true;
+        }
+        assertTrue(exception);
+    }
+
+    @Test
+    public void testCalculateAreaAfterSizeChange() {
+        Square square = new Square(5);
+        rectangle.setSize(7);
+        double area = rectangle.calculateArea();
+        assertEquals(area, 7 * 7, 0.00001);
     }
 }

fb10fc453a8eb40af4da96767403fbfd2b604125
amirmosio <amir.mn.mosio@gmail.com>
2022-05-27 20:55:37 +0430

    Define rectangle class test

--- CodeCoverageProject/src/test/java/shape_management/RectangleTest.java
+++ CodeCoverageProject/src/test/java/shape_management/RectangleTest.java
@@ -1,13 +1,53 @@
 package shape_management;
 
+import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertTrue;
 
 public class RectangleTest {
     @Test
-    public void testCalculateArea() {
+    public void testCalculateAreaWithValidSize() {
+        Rectangle rectangle = new Rectangle(5, 6);
+        double area = rectangle.calculateArea();
+        assertEquals(area, 5 * 6);
+    }
 
+    @Test
+    public void testCalculateAreaWithInvalidWidthSize() {
+        Rectangle rectangle = new Rectangle(-5, 6);
+        boolean exception = false;
+        try {
+            double area = rectangle.calculateArea();
+        } catch (InvalidSizeForCalculateAreaException e) {
+            exception = true;
+        }
+        assertTrue(exception);
+    }
+
+    @Test
+    public void testCalculateAreaWithInvalidHeightSize() {
+        Rectangle rectangle = new Rectangle(5, -6);
+        boolean exception = false;
+        try {
+            double area = rectangle.calculateArea();
+        } catch (InvalidSizeForCalculateAreaException e) {
+            exception = true;
+        }
+        assertTrue(exception);
+    }
+
+    @Test
+    public void testCalculateAreaWithInvalidWidthAndHeightHeightSize() {
+        Rectangle rectangle = new Rectangle(-5, -6);
+        boolean exception = false;
+        try {
+            double area = rectangle.calculateArea();
+        } catch (InvalidSizeForCalculateAreaException e) {
+            exception = true;
+        }
+        assertTrue(exception);
     }
 
 }
